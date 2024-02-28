4e96fffbf70eb848f7a2c241e08189ea780ed06c
pupqa <157424059+pupqa@users.noreply.github.com>
2024-02-28 08:26:27 +0300

    laba5final.java

--- laba5/src/ExpressionCalculator.java
+++ laba5/src/ExpressionCalculator.java
@@ -4,37 +4,52 @@ public class ExpressionCalculator {
         return 3 * x + 5;
     }
     public double var2(double a, double b) {
-        return (a + b) / (a - b);
+        if (a - b == 0) {
+            System.out.println("Ошибка: деление на ноль!");
+            return 0;
+        } else {
+            return (a + b) / (a - b);
+        }
     }
-    public double var3(double a, double x, double b) {
+    public double var3(int a, int x, int b) {
         double result = 1;
-        int factorial = (int) (a * x / b);
-        for (int i = 1; i <= factorial; i++) {
-            result *= i;
+        if (b != 0) {
+            int factorial = a * x / b;
+            for (int i = 1; i <= factorial; i++) {
+                result *= i;
+            }
+        } else {
+            System.out.println("Ошибка: деление на ноль!");
+            return 0;
         }
         return result;
     }
+}
+class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         ExpressionCalculator calculator = new ExpressionCalculator();
-        System.out.print("введите значение 'x' для выражении y=3x+5: ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите значение 'x' для выражения y = 3x + 5: ");
         int x = scanner.nextInt();
         int result1 = calculator.var1(x);
         System.out.println("y = " + result1);
-        System.out.print("введите значение 'a' в выражении y=(a+b)/(a-b): ");
+
+        System.out.print("Введите значение 'a' в выражении y = (a + b) / (a - b): ");
         double a = scanner.nextDouble();
-        System.out.print("введите значение 'b' в выражении y=(a+b)/(a-b): ");
+        System.out.print("Введите значение 'b' в выражении y = (a + b) / (a - b): ");
         double b = scanner.nextDouble();
         double result2 = calculator.var2(a, b);
         System.out.println("y = " + result2);
-        System.out.print("введите значение 'a' в выражении y=(ax/b)!: ");
-        double a3 = scanner.nextDouble();
-        System.out.print("введите значение 'x' в выражении y=(ax/b)!: ");
-        double x3 = scanner.nextDouble();
-        System.out.print("введите значение 'b' в выражении y=(ax/b)!: ");
-        double b3 = scanner.nextDouble();
+
+        System.out.print("Введите значение 'a' в выражении y = (ax / b)!: ");
+        int a3 = scanner.nextInt();
+        System.out.print("Введите значение 'x' в выражении y = (ax / b)!: ");
+        int x3 = scanner.nextInt();
+        System.out.print("Введите значение 'b' в выражении y = (ax / b)!: ");
+        int b3 = scanner.nextInt();
         double result3 = calculator.var3(a3, x3, b3);
         System.out.println("y = " + result3);
-        scanner.close();
     }
 }
+
